9693e3d6fac84d3eb4f0da17426e28befa8741c0
Chapic <antonio.caparroz@outlook.com>
2025-06-11 11:18:39 +0200

    Cambios en carpeta

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
 import App from './App.jsx';
 import Home from './Pages/Home.jsx';
 import About from './Pages/About.jsx';
