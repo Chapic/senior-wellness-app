58ec784b471cb55cb7dbf11d45eedc1d20a34bff
Chapic <antonio.caparroz@outlook.com>
2025-06-11 14:14:13 +0200

    Confirmar

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
+import { Outlet } from 'react-router-dom';
 
-const Layout = ({ children }) => {
+const Layout = () => {
   return (
     <>
       <Header />
-      <main>{children}</main>
+      <main>
+        <Outlet />
+      </main>
       <Footer />
     </>
   );
