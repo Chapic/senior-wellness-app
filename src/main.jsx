f4e1d61f7aaf6d0e1e180474d9425f0ed95044a2
Chapic <antonio.caparroz@outlook.com>
2025-06-11 11:09:45 +0200

    Nuevos cambios

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import App from './App.jsx';
-import Home from './Home.jsx';
-import About from './About.jsx';
+import Home from './Pages/Home.jsx';
+import About from './Pages/About.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
