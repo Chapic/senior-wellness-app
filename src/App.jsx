b5ac47f5e64faa5ee3aa14b571777ab9e218f115
Chapic <antonio.caparroz@outlook.com>
2025-06-11 18:20:00 +0200

    c

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import { Routes, Route } from 'react-router-dom'
+import Layout from './Components/Layout'
+import Home from './Pages/Home'
+import About from './Pages/About'
+import Rutinas from './Pages/Rutinas'
 
 const App = () => {
   return (
-    <div style={{ textAlign: 'center', padding: '2rem' }}>
-      <h1>✅ Funciona: App.jsx está renderizando correctamente</h1>
-    </div>
-  );
-};
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="rutinas" element={<Rutinas />} />
+      </Route>
+    </Routes>
+  )
+}
 
-export default App;
+export default App
