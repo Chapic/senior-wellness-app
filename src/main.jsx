fff1e805647324db24e31072ff5fae55c5243804
Chapic <antonio.caparroz@outlook.com>
2025-06-11 13:36:41 +0200

    modificar

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
+import App from './App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -10,4 +10,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     </BrowserRouter>
   </React.StrictMode>
 );
-// If you want to start measuring performance in your app, pass a function
