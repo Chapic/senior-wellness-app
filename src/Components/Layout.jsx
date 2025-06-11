c46245839477a204b35c05d35725df16db0a2125
Chapic <antonio.caparroz@outlook.com>
2025-06-11 14:22:09 +0200

    si

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
-import { Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom'; // ← esto es lo que te falta
 
 const Layout = () => {
   return (
