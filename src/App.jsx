386628ad98121c2369e11a1fe297343f9a01cd34
Chapic <antonio.caparroz@outlook.com>
2025-06-11 13:37:11 +0200

    modificar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import Layout from './Components/Layout';
 import { Routes, Route } from 'react-router-dom';
+import Layout from './Components/Layout';
 import Home from './Pages/Home';
 import About from './Pages/About';
 import Rutinas from './Pages/Rutinas';
@@ -12,10 +12,14 @@ const App = () => {
     <Layout>
       <div style={{ textAlign: 'center', padding: '1rem' }}>
         <h3>✅ Ejercicios completados: {contador}</h3>
-        <button onClick={() => setContador(contador + 1)} style={{ padding: '0.5rem 1rem', marginBottom: '1rem' }}>
+        <button
+          onClick={() => setContador(contador + 1)}
+          style={{ padding: '0.5rem 1rem', marginBottom: '1rem' }}
+        >
           +1 Ejercicio
         </button>
       </div>
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
